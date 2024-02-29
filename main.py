737667c67b0d874c89e255d78f7cdb1e04c2a2f1
saivenkatesh <saivenkatesh619@gmail.com>
2024-02-29 09:38:02 +0530

    Add credit card payment

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
-df = pd.read_csv("hotels.csv", dtype={'id':str})
+df = pd.read_csv("hotels.csv", dtype={'id': str})
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient='records')
+
 
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
-        self.hotel_name = df.loc[df["id"]==self.hotel_id, 'name'].squeeze()
-        self.hotel_city = df.loc[df["id"]==self.hotel_id, 'city'].squeeze()
+        self.hotel_name = df.loc[df["id"] == self.hotel_id, 'name'].squeeze()
+        self.hotel_city = df.loc[df["id"] == self.hotel_id, 'city'].squeeze()
+
     def book(self):
         """Book a hotel by changing its availabity as no"""
         df.loc[df['id'] == self.hotel_id, 'available'] = 'no'
@@ -16,12 +19,29 @@ class Hotel:
         """Check whether hotel is available"""
         check = df.loc[df['id'] == self.hotel_id]['available'].squeeze()
         if check == 'yes':
-             return True
+            return True
         else:
-             return False
+            return False
+
+
+# Extending Feature
+class Payment:
+    def __init__(self, card, exdate, cvv, holder):
+        self.cc = card
+        self.exp = exdate
+        self.cvv = cvv
+        self.holder = holder
+
+    def payment(self):
+        card_data = {'number': self.cc, 'expiration': self.exp, 'cvc': self.cvv, 'holder': self.holder}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
 
 class Ticket:
-    def __init__(self,name,hotel_obj):
+    def __init__(self, name, hotel_obj):
         self.name = name
         self.hotel = hotel_obj
 
@@ -34,17 +54,23 @@ class Ticket:
         """
         return data
 
+
 print(df)
 hotel_id = input("Select the Hotel: ")
 hotel = Hotel(hotel_id)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter Your Name: ")
-    ticket = Ticket(name, hotel)
-    print(ticket.generate())
+    card_number = "1234"
+    expiry_date = "12/26"
+    cvv = "123"
+    card_holder = "JOHN SMITH"
+    payment = Payment(card_number, expiry_date, cvv, card_holder)
+    if payment.payment():
+        hotel.book()
+        name = input("Enter Your Name: ")
+        ticket = Ticket(name, hotel)
+        print(ticket.generate())
+    else:
+        print("Something went wrong in payment")
 else:
     print("Hotel is Full")
-
-
-
